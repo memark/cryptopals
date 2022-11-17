0008efe2bfc2765f926483d02e42e0f96bb38b29
Magnus Markling <magnus@markling.com>
2022-11-17 16:37:22 +0100

    format code

--- src/set1.rs
+++ src/set1.rs
@@ -4,7 +4,9 @@ mod tests {
 
     #[test]
     fn challenge_1_hex_to_base64() {
-        let result = hex_to_base64("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d");
+        let result = hex_to_base64(
+            "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+        );
         assert_eq!(
             result,
             "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
@@ -46,7 +48,10 @@ mod tests {
     #[test]
     fn challenge_5_repeating_key_xor() {
         assert_eq!(
-            repeating_key_xor("Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal", "ICE"),        
+            repeating_key_xor(
+                "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal",
+                "ICE"
+            ),
             "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"
         );
     }
