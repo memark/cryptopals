d8d6ce584b18fe02a55073f16565e1339f6ac9a5
Magnus Markling <magnus@markling.com>
2022-11-20 22:46:11 +0100

    refactor repeating_key_xor()

--- cryptopals/src/utils.rs
+++ cryptopals/src/utils.rs
@@ -17,14 +17,12 @@ pub fn fixed_xor(a: &[u8], b: &[u8]) -> Vec<u8> {
         .collect::<Vec<u8>>()
 }
 
-pub fn repeating_key_xor(input: &str, key: &str) -> String {
-    let r = input
-        .chars()
-        .zip(key.chars().cycle())
-        .map(|(v, k)| v as u8 ^ k as u8)
-        .collect::<Vec<u8>>();
-
-    hex::encode(r)
+pub fn repeating_key_xor(input: &[u8], key: &[u8]) -> Vec<u8> {
+    input
+        .iter()
+        .zip(key.iter().cycle())
+        .map(|(i, k)| i ^ k)
+        .collect()
 }
 
 pub fn hamming_distance<'a, T>(a: T, b: T) -> u8
@@ -75,10 +73,10 @@ mod tests {
     fn challenge_5_repeating_key_xor() {
         assert_eq!(
             repeating_key_xor(
-                "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal",
-                "ICE"
+                b"Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal",
+                b"ICE",
             ),
-            "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"
+            hex::decode("0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f").unwrap(),
         );
     }
 
