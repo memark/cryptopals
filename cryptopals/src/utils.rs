3384931af93119cf288fd328abb7cbcfcc69108d
Magnus Markling <magnus@markling.com>
2022-11-20 23:01:55 +0100

    refactor test

--- cryptopals/src/utils.rs
+++ cryptopals/src/utils.rs
@@ -40,11 +40,10 @@ mod tests {
 
     #[test]
     fn challenge_1_hex_to_base64() {
-        let result = hex_to_base64(
-            "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
-        );
         assert_eq!(
-            result,
+            hex_to_base64(
+                "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+            ),
             "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
         );
     }
